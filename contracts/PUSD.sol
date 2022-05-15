5a85d3a667a03853c41b637a03e474a703df6ad8
a <a@example.com>
2022-05-16 00:31:59 +0900

    WIP

--- contracts/PUSD.sol
+++ contracts/PUSD.sol
@@ -1,17 +1,33 @@
-// SPDX-License-Identifier: CC0-1.0
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@uniswap/lib/contracts/libraries/FullMath.sol";
+import "@uniswap/lib/contracts/libraries/FixedPoint96.sol";
 import "./IERC4626.sol";
 import "./IStrategy.sol";
+import "../deps/perpdex-contract/contracts/interface/IAccountBalance.sol";
+import "../deps/perpdex-contract/contracts/interface/IClearingHousePerpdex.sol";
+import "../deps/perpdex-contract/contracts/interface/IExchangePerpdex.sol";
+import "../deps/perpdex-contract/contracts/interface/IVault.sol";
+import "../deps/perpdex-contract/contracts/lib/PerpMath.sol";
 
 contract PUSD is IERC4626, ERC20 {
+    using PerpMath for uint160;
+    uint constant Q96 = 0x1000000000000000000000000;
+
     address public override asset;
 
-    constructor(address asset_) ERC20("Perpdex USD", "PUSD") {
+    IClearingHousePerpdex public clearingHouse;
+    IVault public vault;
+    IAccountBalance public accountBalance;
+    IExchangePerpdex public exchange;
+
+    constructor(address asset_, address clearingHouse_) ERC20("Perpdex USD", "PUSD") {
         asset = asset_;
+        clearingHouse = clearingHouse_;
     }
 
     function mint(uint shares, address receiver)
@@ -46,22 +62,23 @@ contract PUSD is IERC4626, ERC20 {
         require(false, "not implemented");
     }
 
-    function rebalance() external {
+    function rebalance() public {
         require(false, "not implemented");
     }
 
     // views
 
     function totalAssets() external view override returns (uint totalManagedAssets) {
-        require(false, "not implemented");
+        int accountValue = clearingHouse.getAccountValue(address(this));
+        return accountValue < 0 ? 0 : uint(accountValue);
     }
 
     function convertToShares(uint assets) external view override returns(uint shares) {
-        require(false, "not implemented");
+        return FullMath.mulDiv(assets, Q96, _getPriceX96());
     }
 
     function convertToAssets(uint shares) external view override returns(uint assets) {
-        require(false, "not implemented");
+        return FullMath.mulDiv(assets, _getPriceX96(), Q96);
     }
 
     function maxDeposit(address receiver) external view override returns(uint maxAssets) {
@@ -94,5 +111,12 @@ contract PUSD is IERC4626, ERC20 {
 
     function previewRedeem(uint shares) public view override returns(uint assets) {
         require(false, "not implemented");
+    }
+
+    // private
+
+    function _getPriceX96() private pure returns(uint) {
+        uint160 sqrtPriceX96 = exchange.getSqrtMarkPriceX96(baseToken);
+        return sqrtPriceX96.formatSqrtPriceX96ToPriceX96();
     }
 }
