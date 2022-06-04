343ea770fe6cc2586032c1ad8667647720cc7c66
a <a@example.com>
2022-06-04 19:22:33 +0900

    minor fixes

--- contracts/PerpdexTokenBase.sol
+++ contracts/PerpdexTokenBase.sol
@@ -6,7 +6,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/SafeCast.sol";
-import {FullMath} from "@uniswap/lib/contracts/libraries/FullMath.sol";
+import { FullMath } from "@uniswap/v3-core/contracts/libraries/FullMath.sol";
 import {IPerpdexExchange} from "../deps/perpdex-contract/contracts/interface/IPerpdexExchange.sol";
 import {IPerpdexMarket} from "../deps/perpdex-contract/contracts/interface/IPerpdexMarket.sol";
 import {IERC4626} from "./interface/IERC4626.sol";
