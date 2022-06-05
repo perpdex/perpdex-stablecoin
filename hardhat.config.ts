6c4a4d6fed9906dd19ac00ccf23fc30566335687
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-05 21:33:24 +0900

    Add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
-import "solidity-coverage"
+import "solidity-coverage";
 
 module.exports = {
   solidity: {
@@ -7,8 +8,12 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    // externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
 };
