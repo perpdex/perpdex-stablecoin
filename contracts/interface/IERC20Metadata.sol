bfdb8fd6e4446f0eefc941642bf1c460c0516a8b
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-05 21:33:59 +0900

    Fix IERC20Metadata inherit

--- contracts/interface/IERC20Metadata.sol
+++ contracts/interface/IERC20Metadata.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.7.6;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IERC20Metadata {
+interface IERC20Metadata is IERC20 {
     function symbol() external view returns (string memory);
 }
