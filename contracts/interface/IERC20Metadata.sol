3afb5a5c9ebe26f876313b26eaf1998ac4929fed
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-07 03:21:11 +0900

    Fix interface

--- contracts/interface/IERC20Metadata.sol
+++ contracts/interface/IERC20Metadata.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.7.6;
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IERC20Metadata is IERC20 {
+interface IERC20Metadata {
     function name() external view returns (string memory);
 
     function symbol() external view returns (string memory);
