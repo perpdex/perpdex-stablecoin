8e624db93de1e7626dc152211676d6ef506b5c86
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-07 17:22:54 +0900

    Add maxWithdraw tests

--- test/longToken/withdraw.test.ts
+++ test/longToken/withdraw.test.ts
@@ -46,4 +46,34 @@ describe("PerpdexLongToken.withdraw", async () => {
     function parseShares(amount: string) {
         return parseUnits(amount, longTokenDecimals)
     }
+
+    describe("maxWithdraw", async () => {
+        beforeEach(async () => {
+            // approve max
+            await weth.approveForce(alice.address, longToken.address, ethers.constants.MaxUint256)
+            await weth.approveForce(longToken.address, exchange.address, ethers.constants.MaxUint256)
+        })
+        ;[
+            {
+                title: "return owner's assets when pool has some liquidity",
+                pool: {
+                    base: "100",
+                    quote: "100",
+                },
+                depositAmount: "1",
+                expected: "1.009999999999999999",
+            },
+        ].forEach(test => {
+            it(test.title, async () => {
+                // init pool
+                await initPool(exchange, market, owner, parseShares(test.pool.base), parseAssets(test.pool.base))
+
+                // alice deposits
+                await weth.connect(owner).mint(alice.address, parseAssets(test.depositAmount))
+                await longToken.connect(alice).deposit(parseAssets(test.depositAmount), alice.address)
+
+                expect(await longToken.maxWithdraw(alice.address)).to.eq(parseAssets(test.expected))
+            })
+        })
+    })
 })
