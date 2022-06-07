28d93cf3f59f2fe332b00fade1a1639ad72f5d31
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-08 00:57:43 +0900

    Add maxRedeem tests

---
+++ test/longToken/redeem.test.ts
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+import { MockContract } from "@defi-wonderland/smock"
+import { expect } from "chai"
+import { Wallet } from "ethers"
+import { parseUnits } from "ethers/lib/utils"
+import { ethers, waffle } from "hardhat"
+import { PerpdexLongToken, TestERC20, TestPerpdexExchange, TestPerpdexMarket } from "../../typechain"
+import { createPerpdexExchangeFixture } from "./fixtures"
+import { initPool } from "./helpers"
+
+describe("PerpdexLongToken redeem", async () => {
+    let loadFixture = waffle.createFixtureLoader(waffle.provider.getWallets())
+    let fixture
+
+    let longToken: PerpdexLongToken
+    let longTokenMock: MockContract<PerpdexLongToken>
+    let longTokenDecimals: number
+    let market: TestPerpdexMarket
+    let exchange: TestPerpdexExchange
+    let weth: TestERC20
+    let wethDecimals: number
+    let owner: Wallet
+    let alice: Wallet
+    let bob: Wallet
+
+    beforeEach(async () => {
+        fixture = await loadFixture(createPerpdexExchangeFixture())
+
+        longToken = fixture.perpdexLongToken
+        longTokenMock = fixture.perpdexLongTokenMock
+        longTokenDecimals = await longToken.decimals()
+        market = fixture.perpdexMarket
+        exchange = fixture.perpdexExchange
+
+        weth = fixture.weth
+        wethDecimals = await weth.decimals()
+
+        owner = fixture.owner
+        alice = fixture.alice
+        bob = fixture.bob
+    })
+
+    function parseAssets(amount: string) {
+        return parseUnits(amount, wethDecimals)
+    }
+
+    function parseShares(amount: string) {
+        return parseUnits(amount, longTokenDecimals)
+    }
+
+    describe("maxRedeem", async () => {
+        beforeEach(async () => {
+            // approve max
+            await weth.approveForce(alice.address, longToken.address, ethers.constants.MaxUint256)
+            await weth.approveForce(longToken.address, exchange.address, ethers.constants.MaxUint256)
+        })
+        ;[
+            {
+                title: "return owner's shares",
+                pool: {
+                    base: "10000",
+                    quote: "10000",
+                },
+                depositAssets: "10",
+                expected: "9.990009990009990009",
+            },
+        ].forEach(test => {
+            it(test.title, async () => {
+                // init pool
+                await initPool(exchange, market, owner, parseShares(test.pool.base), parseAssets(test.pool.base))
+
+                // alice deposits
+                await weth.connect(owner).mint(alice.address, parseAssets(test.depositAssets))
+                await longToken.connect(alice).deposit(parseAssets(test.depositAssets), alice.address)
+
+                // alice maxRedeem
+                expect(await longToken.maxRedeem(alice.address)).to.eq(parseAssets(test.expected))
+            })
+        })
+    })
+})
