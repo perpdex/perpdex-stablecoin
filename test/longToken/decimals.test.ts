0c1f0cecfee8987493218ede10ff3ee7536558e3
takamiml <91838137+takami-ml@users.noreply.github.com>
2022-06-12 13:49:49 +0900

    Add balanceOf decimals tests

--- test/longToken/decimals.test.ts
+++ test/longToken/decimals.test.ts
@@ -12,14 +12,21 @@ describe("PerpdexLongToken base decimals", async () => {
     let fixture
 
     let longToken: PerpdexLongToken
-    let longTokenDecimals: number
     let market: TestPerpdexMarket
     let exchange: TestPerpdexExchange
     let weth: TestERC20
-    let wethDecimals: number
     let owner: Wallet
     let alice: Wallet
     let bob: Wallet
+
+    function toWallet(who: string) {
+        if (who === "alice") {
+            return alice
+        }
+        if (who === "bob") {
+            return bob
+        }
+    }
 
     async function before(decimals: number = 18) {
         fixture = await loadFixture(
@@ -29,12 +36,10 @@ describe("PerpdexLongToken base decimals", async () => {
         )
 
         longToken = fixture.perpdexLongToken
-        longTokenDecimals = await longToken.decimals()
         market = fixture.perpdexMarket
         exchange = fixture.perpdexExchange
 
         weth = fixture.weth
-        wethDecimals = await weth.decimals()
 
         owner = fixture.owner
         alice = fixture.alice
@@ -42,11 +47,6 @@ describe("PerpdexLongToken base decimals", async () => {
 
         // alice approve longToken of max assets
         await weth.connect(alice).approve(longToken.address, ethers.constants.MaxUint256)
-    }
-
-    // for assets
-    function parseAssets(amount: string) {
-        return parseUnits(amount, wethDecimals)
     }
 
     // for shares, perpdex base and quote
@@ -62,7 +62,16 @@ describe("PerpdexLongToken base decimals", async () => {
                 quote: "10000",
             },
             isMarketAllowed: true,
-            depositAssets: parseUnits("100", 6),
+
+            deposit: {
+                assets: parseUnits("100", 6),
+                receiver: "alice",
+            },
+
+            balanceOf: {
+                owner: "alice",
+                expected: parseUnits("99.009900990099009900", 18),
+            },
 
             totalSupply: {
                 expected: parseUnits("99.009900990099009900", 18),
@@ -110,10 +119,12 @@ describe("PerpdexLongToken base decimals", async () => {
                 await initPool(fixture, parse18(test.pool.base), parse18(test.pool.quote))
 
                 // alice deposit to perpdex
-                if (test.depositAssets.gt(0)) {
+                if (test.deposit !== void 0 && test.deposit.assets.gt(0)) {
                     // mint balance
-                    await weth.connect(owner).mint(alice.address, test.depositAssets)
-                    await longToken.connect(alice).deposit(test.depositAssets, alice.address)
+                    await weth.connect(owner).mint(alice.address, test.deposit.assets)
+                    // deposit
+                    var receiver = toWallet(test.deposit.receiver)
+                    await longToken.connect(alice).deposit(test.deposit.assets, receiver.address)
                 }
 
                 // change market allowance
@@ -122,6 +133,11 @@ describe("PerpdexLongToken base decimals", async () => {
 
             it("asset", async () => {
                 expect(await longToken.asset()).to.eq(await exchange.settlementToken())
+            })
+
+            it("balanceOf", async () => {
+                var owner_ = toWallet(test.balanceOf.owner)
+                expect(await longToken.balanceOf(owner_.address)).to.eq(test.balanceOf.expected)
             })
 
             it("totalSupply", async () => {
